e22449fb51e7797fd4183366253973d54f695f1b
Roeland Weve <roeland.weve@justpinch.com>
2012-06-27 13:09:03 +0200

    Fixed error in redefinition of URL

--- Classes/ShareKit/Core/Base Sharer Classes/SHKOAuthSharer.m
+++ Classes/ShareKit/Core/Base Sharer Classes/SHKOAuthSharer.m
@@ -146,14 +146,11 @@
 	// some URLs already have ? in it (with Hyves for example)
 	NSURL *url;
 	
-	if([authorizeURL.absoluteString rangeOfString:@"?"].location == NSNotFound)
+	if([authorizeURL.absoluteString rangeOfString:@"?"].location == NSNotFound) {
 		url = [NSURL URLWithString:[NSString stringWithFormat:@"%@?oauth_token=%@", authorizeURL.absoluteString, requestToken.key]];
-	else 
+	} else { 
 		url = [NSURL URLWithString:[NSString stringWithFormat:@"%@&oauth_token=%@", authorizeURL.absoluteString, requestToken.key]];
-    
-	//NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@?oauth_token=%@", authorizeURL.absoluteString, requestToken.key]];
-	
-	NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@?oauth_token=%@", authorizeURL.absoluteString, requestToken.key]];
+	}
 	
 	SHKOAuthView *auth = [[SHKOAuthView alloc] initWithURL:url delegate:self];
 	[[SHK currentHelper] showViewController:auth];	
